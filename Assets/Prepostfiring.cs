1cc35b712b185da150d97a6ab653a233dc823787
Adam <adamdials@unity3d.com>
2015-08-12 17:15:02 -0500

    2nd

---
+++ Assets/Prepostfiring.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+
+        public static void PreExport()
+        {
+
+            Console.WriteLine("From inside the project: This is a pre-export method firing");
+
+
+        }
+
+        public static void PostExport(string path)
+        {
+
+            Console.WriteLine("From inside the project: This is a post-export method firing, and this is the path we ingested: " + path);
+
+        }
+
+    }
+}
+
